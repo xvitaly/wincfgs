b96d9e7c2ba43af7f4e1fd80184f714df74294ca
Vitaly Zaitsev <vitaly@easycoding.org>
2020-12-10 18:42:21 +0100

    Unified the Edge uninstaller script.

--- scripts/edge_uninstall.cmd
+++ scripts/edge_uninstall.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-set EDGE_VERSION=85.0.564.67
+title Uninstalling Edge...
 
-"%PROGRAMFILES(X86)%\Microsoft\Edge\Application\%EDGE_VERSION%\Installer\setup.exe" --uninstall --system-level --verbose-logging --force-uninstall
+for /D %%I in ("%PROGRAMFILES(X86)%\Microsoft\Edge\Application\*") do "%%~I\Installer\setup.exe" --uninstall --system-level --verbose-logging --force-uninstall
