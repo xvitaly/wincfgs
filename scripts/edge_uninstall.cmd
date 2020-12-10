d27866885af525fb8051b4122e0a8af7bc88b16a
Vitaly Zaitsev <vitaly@easycoding.org>
2020-12-10 18:21:12 +0100

    Updated Edge uninstaller.

--- scripts/edge_uninstall.cmd
+++ scripts/edge_uninstall.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-set EDGE_VERSION=84.0.522.69
+set EDGE_VERSION=85.0.564.67
 
 "%PROGRAMFILES(X86)%\Microsoft\Edge\Application\%EDGE_VERSION%\Installer\setup.exe" --uninstall --system-level --verbose-logging --force-uninstall
