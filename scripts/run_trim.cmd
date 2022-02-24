287b0ef93fa56a5c355df9c8aef7b2f2e15c49ce
Vitaly Zaitsev <vitaly@easycoding.org>
2022-02-24 13:26:42 +0100

    Updated run_trim.cmd script to use environment variable instead of hardcoded drive letter.

--- scripts/run_trim.cmd
+++ scripts/run_trim.cmd
@@ -1,5 +1,4 @@
 @echo off
 
 title Performing TRIM on SSD drives...
-powershell -command "Optimize-Volume -DriveLetter C -ReTrim -Verbose"
-powershell -command "Optimize-Volume -DriveLetter D -ReTrim -Verbose"
+powershell -command "Optimize-Volume -DriveLetter %SystemDrive% -ReTrim -Verbose"
